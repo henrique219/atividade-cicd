1d34a54c7763d7d5c61814a1e39840161ec30e4e
henrique <henriquesivaldo9@gmail.com>
2026-09-08 20:50:16 -0300

    Cria aplicação com testes automatizados

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+  const [cliques, setCliques] = useState(0)
+
+  return (
+    <main>
+      <h1>Minha aplicação com CI/CD</h1>
+
+      <p>Cliques: {cliques}</p>
+
+      <button onClick={() => setCliques(cliques + 1)}>
+        Adicionar clique
+      </button>
+    </main>
+  )
+}
+
+export default App
